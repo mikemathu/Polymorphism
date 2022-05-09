2dcf95d6012c583fa9e690eedbb6f84bbb7ccb1f
Michael Mathu <mathumichael17@gmail.com>
2022-05-09 12:59:45 +0300

    initial commit

---
+++ GoogleSearch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class GoogleSearch : SearchEngine
+    {
+
+        public override string[] Search(string findThis)
+        {
+            // Google Search is, of course, way better than 3 hard-coded results like this...
+            return new string[] {
+                "Here are some great results.",
+                   "Aren't they neat?",
+                "I found 1.2 billion things, but you will only look at the first 10." 
+            };
+
+            
+        
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using Polymorphism;
+//Base Class
+class SearchEngine
+{
+    static void Main(string[] args)
+    {
+        GoogleSearch googleSearch = new GoogleSearch();
+        googleSearch.Search("");
+
+     
+    }
+    public virtual string[] Search(string findThis)
+    {
+        return new string[0]; // I'm terrible at searching... I give up.
+    }
+
+}
